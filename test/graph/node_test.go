5f135ec156963d71f3b9365e59f20b531d8f8e7a
Vitor Della Torre dos Santos <vitor.origamer@gmail.com>
2020-07-15 15:48:38 -0300

    Added test for nodes

--- test/graph/node_test.go
+++ test/graph/node_test.go
@@ -1,1 +1,22 @@
 package graph
+
+import (
+	"parzival/graphs-101/src/graph"
+	"testing"
+)
+
+const expected = "Expected %v, got %v"
+
+func Test_element(t *testing.T) {
+	testNode := graph.NewNode(1)
+	if testNode.GetElement() != 1 {
+		t.Errorf(expected, 1, testNode.GetElement())
+	}
+}
+
+func Test_zero_neighbors(t *testing.T) {
+	testNode := graph.NewNode(1)
+	if len(testNode.GetNeighbors()) != 0 {
+		t.Errorf(expected, true, len(testNode.GetNeighbors()))
+	}
+}
